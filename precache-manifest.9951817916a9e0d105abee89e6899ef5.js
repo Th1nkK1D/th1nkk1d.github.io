a976c42ba994d83c5a7896f3718ade0add89f783
Withee Poositasai <witheep@gmail.com>
2019-04-17 15:10:54 +0700

    fix menifest and favicon

---
+++ precache-manifest.9951817916a9e0d105abee89e6899ef5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "4630274334d069e504a0",
+    "url": "/css/app.97debd02.css"
+  },
+  {
+    "revision": "4630274334d069e504a0",
+    "url": "/js/app.d5b4cabb.js"
+  },
+  {
+    "revision": "da98ab81bca0d37a14a5",
+    "url": "/js/chunk-vendors.414e6504.js"
+  },
+  {
+    "revision": "718c87de7a98bbfc716afe803d3645aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd16a726c6f29210dcad73f06c96720e",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "aa0aea89a06d0497092dc502861d38f7",
+    "url": "/svg/codepen.min.svg"
+  },
+  {
+    "revision": "080b78ceed826472d284496361ed035a",
+    "url": "/svg/balloonicorn2.min.svg"
+  },
+  {
+    "revision": "22057cb8477e0433ece1310c1187e148",
+    "url": "/img/lkavatar.jpg"
+  },
+  {
+    "revision": "36429f3c47a2bf680d4d87ad03b9b9e9",
+    "url": "/img/fb_preview.jpg"
+  }
+];
